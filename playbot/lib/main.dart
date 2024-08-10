d69a2eb5a8cdeb368deaf74305f92b2794f80b08
Abhijeet Dash <ad@Abhijeets-MacBook-Pro.local>
2024-08-10 22:35:17 +0530

    Added firebase project

--- playbot/lib/main.dart
+++ playbot/lib/main.dart
@@ -1,12 +1,14 @@
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:playbot/application/application.dart';
+import 'package:playbot/firebase_options.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const Playbot());
 }
